afae0c4cc46c803c9f3717ee9502cd8549cb6007
Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2023-04-30 12:31:58 +0500

    positioned widget

---
+++ widgets_practice/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(flutter_widgets());
+}
+class flutter_widgets extends StatelessWidget {
+  const flutter_widgets({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title:'Positioned Widget',
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: positionedwidget()
+    );
+  }
+}
+
+class positionedwidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Positioned Widget'),
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              bottom: 12,
+              left: 10,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.deepPurple,
+              ),
+            )
+          ],
+        )
+    );
+  }
+}
+
